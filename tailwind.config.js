ad2f57af31360db28093253d48fe9c3bc3e96224
sabitmohamad <mohamadlaitssabit@gmail.com>
2022-09-25 21:42:37 +0700

    Company Profile

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    container: {
+      center: "true",
+      padding: "40px",
+    },
+    extend: {
+      backgroundImage: {},
+    },
+  },
+  plugins: [],
+};
